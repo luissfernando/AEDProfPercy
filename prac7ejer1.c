92af4238f9a717b0b773b7c3536a83d69486f8d3
luissfernando <lmoralest@unsa.edu.pe>
2025-05-02 10:00:17 -0500

    ejercicio resuelto

--- prac7ejer1.c
+++ prac7ejer1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 int* select_positive(const int* v, int n, int* size_positives) {
-    // 
+    //  for para saber el tamaño del primer arreglo
     for (int i = 0; i < n; i++) {
         if (v[i] >= 0) {
             (*size_positives)++; // *size_positives++
         }
     }
+    //si el tamaño del segundo arreglo es mayor a 0 entonses hay numeros positivos
     if(*size_positives!=0){
         int *positives = malloc(*size_positives *(sizeof(int)));
         for(int i=0,j=0;i<n;i++){
@@ -15,18 +16,16 @@ int* select_positive(const int* v, int n, int* size_positives) {
                 j++;
             }
         }
-        printf("%d\n ",*size_positives);
         return positives;
     }
 }
-
 int main() {
     int size_n=6; // tamaño del primer arreglo
     int v[] = {-1, 2, 0, 3, -5, 7};
     int size_positives=0; //tamaño del arreglo de positivos
 
     int* positives = select_positive(v, size_n, &size_positives);
-
+    
     if(positives != NULL) {
         printf("%d\n ",size_positives);
         printf("Numeros posistivos : ");
