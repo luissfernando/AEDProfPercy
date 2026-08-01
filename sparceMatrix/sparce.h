3a18cb0290292a4e2b02829b08a9d5cd73b025d5
luissfernando <lmoralest@unsa.edu.pe>
2026-08-01 11:08:18 -0500

    sparceM

--- sparceMatrix/sparce.h
+++ sparceMatrix/sparce.h
@@ -1,70 +1,63 @@
 #ifndef SPARCE_H
 #define SPARCE_H
 
-#include "node.h"  // Asegúrate de incluir el archivo de nodos
+#include "node.h"
 #include <iostream>
+
 template <class T>
 class Sparce {
 public:
     Node<T>** v_columns;
     Node<T>** v_rows;
-    unsigned rows_size, cols_size; 
 
-    
-    Sparce(unsigned x, unsigned y);
-    bool find(Node<T>**& p, Node<T>**& q, int x, int y, T data);
-    void insert(int pos_x, int pos_y, T data);
+    unsigned rows_size;
+    unsigned cols_size;
+
+    Sparce(unsigned cols, unsigned rows);
+
+    void insert(int x, int y, T data);
+
     void mostrar();
 };
 
 template <class T>
-Sparce<T>::Sparce(unsigned x, unsigned y) {
-    v_columns = new Node<T>*[x];
-    v_rows = new Node<T>*[y];
-    cols_size = x;
-    rows_size = y;
-    
+Sparce<T>::Sparce(unsigned cols, unsigned rows) {
+
+    cols_size = cols;
+    rows_size = rows;
+
+    v_columns = new Node<T>*[cols_size];
+    v_rows = new Node<T>*[rows_size];
+
+    for(unsigned i=0;i<cols_size;i++)
+        v_columns[i]=nullptr;
+
+    for(unsigned i=0;i<rows_size;i++)
+        v_rows[i]=nullptr;
 }
 
-template <class T>
-bool Sparce<T>::find(Node<T>**& p, Node<T>**& q, int x, int y, T data) {
-    p = &(v_columns[x]);
-    q = &(v_rows[y]);
-    while (*p && (*p)->data != data) {
-        *p = (*p)->right;
-    }
-    while (*q && (*q)->data != data) {
-        *q = (*q)->down;
-    }
-    return true;
+template<class T>
+void Sparce<T>::insert(int x,int y,T data){
+    Node<T>* nuevo=new Node<T>(data);
+    nuevo->right=v_rows[y];
+    v_rows[y]=nuevo;
+    nuevo->down=v_columns[x];
+    v_columns[x]=nuevo;
 }
 
-template <class T>
-void Sparce<T>::insert(int pos_x, int pos_y, T data) {
-    Node<T> **p, **q;
-    Node<T>* nuevo = new Node<T>(data);
-    find(p, q, pos_x, pos_y, data);
-    nuevo->right = *p;
-    nuevo->down = *q;
-    *p = nuevo;
-    *q = nuevo;
-}
-template <class T>
-void Sparce<T>::mostrar() {
-    for (unsigned i = 0; i < rows_size; ++i) {
-        Node<T>* cabecera = v_rows[i];
-        Node<T>* actual = cabecera->right;
-
-        std::cout << "Fila " << i << ": ";
-
-        // Recorremos hasta volver al nodo cabecera
-        while (actual != cabecera) {
-            std::cout << actual->data << " -> ";
-            actual = actual->right;
+template<class T>
+void Sparce<T>::mostrar(){
+    for(unsigned i=0;i<rows_size;i++){
+        std::cout<<"Fila "<<i<<": ";
+        Node<T>* actual=v_rows[i];
+        while(actual){
+            std::cout<<actual->data;
+            actual=actual->right;
+            if(actual)
+                std::cout<<" -> ";
         }
-
-        std::cout << "circular\n";
+        std::cout<<std::endl;
     }
 }
 
-#endif // SPARCE_H
+#endif
